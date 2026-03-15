dbdc84830e7ae3f3ab5b0ac74e2adf7d94bfb787
RAJ PATEL <rajpatel230106@gmail.com>
2026-03-15 15:57:23 +0530

    ready to deploy

--- app.py
+++ app.py
@@ -79,6 +79,9 @@ def face_match():
     })
 
 
+if __name__ == "__main__": 
+    app.run( host="0.0.0.0", port=5000, debug=False, use_reloader=False )
+
 # python -m venv venv
 # venv\scripts\activate
 # pip install -r requirements.txt
